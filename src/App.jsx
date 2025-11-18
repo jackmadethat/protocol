a76e427741670f82feb0526269b184a5b2cd127e
jackmadethat <underscore@gmx.com>
2025-11-18 21:12:36 +1030

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <div className="centerIcon">
         <img className="icon" src="https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/refs/heads/main/backup/img/animus/icon_nomad.png" />
       </div>
+      <div className="innerIcon"></div>
     </>
   )
 }
