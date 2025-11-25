abb251ef24f8de77f995a268c04e0976974059c2
jackmadethat <underscore@gmx.com>
2025-11-25 21:31:09 +1030

    Fixed links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ const Navigation = ({ handleSystemClick, handleTimerClick }) => {
   return (
     <nav className="navigation">
       <h2>PRTCL</h2>
-      <img src="/src/assets/img/icon_timer.png" className="timerIcon"  onClick={handleTimerClick} />
+      <img src="https://raw.githubusercontent.com/jackmadethat/protocol/refs/heads/main/src/assets/img/icon_timer.png" className="timerIcon"  onClick={handleTimerClick} />
       <img src="https://raw.githubusercontent.com/jackmadethat/protocol/refs/heads/main/src/assets/img/icon_settings.png" className="settingsIcon"  onClick={handleSystemClick} />
       <div></div>
     </nav>
